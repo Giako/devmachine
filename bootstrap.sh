e606956dafad877a660f5e4a8a60e71cc2a3d8ec
Giacomo Russo <gi.russo@reply.it>
2016-10-24 11:16:32 +0200

    Added Node.js 6.x support

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-
 apt-get update
-apt-get install -y vim
+apt-get install -y vim build-essential curl mongodb git
+
+# Install last stable version of Node.js
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+apt-get install -y nodejs
